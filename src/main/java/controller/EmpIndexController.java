a9503fbb3ca5a68b73eb9745bac0b66708b76043
GD <GD@DESKTOP-LCQARK2>
2025-11-03 17:45:12 +0900

    오타수정

--- src/main/java/controller/EmpIndexController.java
+++ src/main/java/controller/EmpIndexController.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/empIndex")
+@WebServlet("/emp/empIndex")
 public class EmpIndexController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/view/emp/empIndex.jsp").forward(request, response);
